1244dd249eeeb8bd35de2a6ce39407365d555427
Balbir Singh <bsingharora@gmail.com>
2017-03-03 14:28:48 +1100

    From: Jérôme Glisse <jglisse@redhat.com> Date: Fri, 19 Aug 2016 15:15:35 -0400 Subject: [PATCH 03/18] mm/ZONE_DEVICE/unaddressable: add support for  un-addressable device memory v3
    This add support for un-addressable device memory. Such memory is hotpluged
    only so we can have struct page but we should never map them as such memory
    can not be accessed by CPU. For that reason it uses a special swap entry for
    CPU page table entry.
    
    This patch implement all the logic from special swap type to handling CPU
    page fault through a callback specified in the ZONE_DEVICE pgmap struct.
    
    Architecture that wish to support un-addressable device memory should make
    sure to never populate the kernel linar mapping for the physical range.
    
    This feature potentially breaks memory hotplug unless every driver using it
    magically predicts the future addresses of where memory will be hotplugged.
    
    Changed since v2:
      -  Do not change devm_memremap_pages()
    Changed since v1:
      - Add unaddressable memory resource descriptor enum
      - Explain why memory hotplug can fail because of un-addressable memory
    
    Signed-off-by: Jérôme Glisse <jglisse@redhat.com>
    Cc: Dan Williams <dan.j.williams@intel.com>
    Cc: Ross Zwisler <ross.zwisler@linux.intel.com>
    
    [Balbir - ported to v4.10]
    Signed-off-by: Balbir Singh <bsingharora@gmail.com>

--- kernel/memremap.c
+++ kernel/memremap.c
@@ -18,6 +18,8 @@
 #include <linux/io.h>
 #include <linux/mm.h>
 #include <linux/memory_hotplug.h>
+#include <linux/swap.h>
+#include <linux/swapops.h>
 
 #ifndef ioremap_cache
 /* temporary while we convert existing ioremap_cache users to memremap */
@@ -200,6 +202,21 @@ void put_zone_device_page(struct page *page)
 }
 EXPORT_SYMBOL(put_zone_device_page);
 
+#if IS_ENABLED(CONFIG_DEVICE_UNADDRESSABLE)
+int device_entry_fault(struct vm_area_struct *vma,
+		       unsigned long addr,
+		       swp_entry_t entry,
+		       unsigned flags,
+		       pmd_t *pmdp)
+{
+	struct page *page = device_entry_to_page(entry);
+
+	BUG_ON(!page->pgmap->page_fault);
+	return page->pgmap->page_fault(vma, addr, page, flags, pmdp);
+}
+EXPORT_SYMBOL(device_entry_fault);
+#endif /* CONFIG_DEVICE_UNADDRESSABLE */
+
 static void pgmap_radix_release(struct resource *res)
 {
 	resource_size_t key, align_start, align_size, align_end;
@@ -255,7 +272,7 @@ static void devm_memremap_pages_release(struct device *dev, void *data)
 
 	lock_device_hotplug();
 	mem_hotplug_begin();
-	arch_remove_memory(align_start, align_size, MEMORY_DEVICE);
+	arch_remove_memory(align_start, align_size, pgmap->flags);
 	mem_hotplug_done();
 	unlock_device_hotplug();
 
@@ -334,6 +351,8 @@ void *devm_memremap_pages(struct device *dev, struct resource *res,
 	}
 	pgmap->ref = ref;
 	pgmap->res = &page_map->res;
+	pgmap->flags = MEMORY_DEVICE;
+	pgmap->page_fault = NULL;
 	pgmap->page_free = NULL;
 	pgmap->data = NULL;
 
@@ -374,7 +393,7 @@ void *devm_memremap_pages(struct device *dev, struct resource *res,
 
 	lock_device_hotplug();
 	mem_hotplug_begin();
-	error = arch_add_memory(align_start, align_size, MEMORY_DEVICE);
+	error = arch_add_memory(align_start, align_size, pgmap->flags);
 	mem_hotplug_done();
 	unlock_device_hotplug();
 	if (error)
